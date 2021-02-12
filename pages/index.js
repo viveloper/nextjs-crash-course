f1c15aaa06c080582fbbb210ee5cdcc3394d19b0
viveloper <ucsik1234@gmail.com>
2021-02-12 22:17:21 +0900

    added Layout

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <>
+    <div>
       <Head>
-        <title>WebDev Newz</title>
+        <title>WebDev News</title>
         <meta name="keywords" content="web development, programming" />
       </Head>
-      <h1>Home</h1>
-    </>
+      <h1>Welcome to Next</h1>
+    </div>
   );
 }
